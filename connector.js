fecce94ce8d30d94953261207171e0588218e714
virgilvox <moheeb.zara@citrix.com>
2015-04-27 17:24:57 -0700

    add to git

---
+++ connector.js
@@ -0,0 +1,78 @@
+'use strict';
+var Plugin = require('./index').Plugin;
+var meshblu = require('meshblu');
+var RemoteIO = require('remote-io');
+var SkynetSerialPort = require('meshblu-vserial').SerialPort;
+var five = require("johnny-five")
+
+var Connector = function(config) {
+  var conx = meshblu.createConnection({
+    server : config.server,
+    port   : config.port,
+    uuid   : config.uuid,
+    token  : config.token
+  });
+
+  var consoleError = function(error) {
+    console.error(error.message);
+    console.error(error.stack);
+  };
+
+  process.on('uncaughtException', consoleError);
+  conx.on('notReady', consoleError);
+  conx.on('error', consoleError);
+
+  var plugin = new Plugin();
+
+  conx.on('ready', function(){
+    conx.whoami({uuid: config.uuid}, function(device){
+      plugin.setOptions(device.options || {});
+      conx.update({
+        uuid: config.uuid,
+        token: config.token,
+        messageSchema: plugin.messageSchema,
+        optionsSchema: plugin.optionsSchema,
+        options:       plugin.options
+      });
+    });
+
+//virtual port
+  var ssp = new SkynetSerialPort(conx, "*");
+
+  var io = new five.Board();
+
+    //virtual serialport + any io
+    var remoteio = new RemoteIO({
+      serial: ssp,
+      io: io
+    });
+
+
+  });
+
+  conx.on('message', function(){
+    try {
+      plugin.onMessage.apply(plugin, arguments);
+    } catch (error){
+      console.error(error.message);
+      console.error(error.stack);
+    }
+  });
+
+  conx.on('config', function(){
+    try {
+      plugin.onConfig.apply(plugin, arguments);
+    } catch (error){
+      console.error(error.message);
+      console.error(error.stack);
+    }
+  });
+
+  plugin.on('message', function(message){
+    conx.message(message);
+  });
+
+  plugin.on('error', consoleError);
+};
+
+module.exports = Connector;
---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var debug = require('debug')('meshblu-arduino')
+
+var MESSAGE_SCHEMA = {
+  type: 'object',
+  properties: {
+    exampleBoolean: {
+      type: 'boolean',
+      required: true
+    },
+    exampleString: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+var OPTIONS_SCHEMA = {
+  type: 'object',
+  properties: {
+    port: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+function Plugin(){
+  this.options = {};
+  this.messageSchema = MESSAGE_SCHEMA;
+  this.optionsSchema = OPTIONS_SCHEMA;
+  return this;
+}
+util.inherits(Plugin, EventEmitter);
+
+Plugin.prototype.onMessage = function(message){
+  var payload = message.payload;
+  this.emit('message', {devices: ['*'], topic: 'echo', payload: payload});
+};
+
+Plugin.prototype.onConfig = function(device){
+  this.setOptions(device.options||{});
+};
+
+Plugin.prototype.setOptions = function(options){
+  this.options = options;
+};
+
+module.exports = {
+  messageSchema: MESSAGE_SCHEMA,
+  optionsSchema: OPTIONS_SCHEMA,
+  Plugin: Plugin
+};
